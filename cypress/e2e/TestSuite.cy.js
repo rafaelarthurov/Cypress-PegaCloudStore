7457fc6f744cca9fdb964cb5a0fdadbdc085336a
rafaelarthurov <rafael.arthurov@gmail.com>
2023-01-05 20:55:12 -0400

    Some CSS assertionson login page

--- cypress/e2e/TestSuite.cy.js
+++ cypress/e2e/TestSuite.cy.js
@@ -14,44 +14,62 @@ var pass = '12345'
     })
 })*/
 
-describe('Login on Pega Cloud', ()=>{
-    before (()=>{
-        //TodoCypress.Cookies.debug(true)
-        cy.visit('/')
-            .should('be.visible')
-    })
-    it('Login on PegaCloud', ()=>{
-        cy.login_pega(user, pass)
-    })
-})
+describe('Pega Cloud e2e test', () =>{
 
-describe('HomePage validations and actions', ()=>{
-  it('Validate the News Alert message', ()=>{
-        cy.fixture('homePage.json').then((homePage)=>{
-            cy.get(homePage.NewsAlert)
-                .contains('All packages shipped via DHL on May 20, 2013 experienced delayed delivery dates of up to 24 hours.')
+    //###################################################
+    context('Login on Pega Cloud', ()=>{
+        before (()=>{
+            cy.visit('/')
+        })
+        it('Validating CSS properties', ()=>{
+            cy.fixture('login.json').then((login)=>{
+                cy.get(login.PageLogo)
+                    .should('have.css', 'width', '100px')
+                    .and('have.css', 'height', '47px')
+                cy.get(login.LoginHeader)
+                    .should('have.css', 'background-color', 'rgb(31, 37, 85)')
+            })                
+        })
+        it('Login on PegaCloud', ()=>{
+            cy.login_pega(user, pass)
         })
     })
-    it("Do a HomePage's product searching", ()=>{
-        cy.reload()
-        cy.homePage_search('1', '24')
+
+    //###################################################
+    
+    context('HomePage validations and actions', ()=>{
+        it('Validate the News Alert message', ()=>{
+              cy.fixture('homePage.json').then((homePage)=>{
+                  cy.get(homePage.NewsAlert)
+                      .contains('All packages shipped via DHL on May 20, 2013 experienced delayed delivery dates of up to 24 hours.')
+              })
+          })
+          it("Do a HomePage's product searching", ()=>{
+              cy.reload()
+              cy.homePage_search('1', '24')
+          })  
+      })
+
+    //###################################################
+
+    context('Product Details validations and actions', ()=>{
+        it('Validate some product value', ()=>{
+            cy.fixture('ProductDetails.json').then((ProductDetails)=>{
+                cy.get(ProductDetails.ProductName)
+                    .first()
+                    .contains('Guarana Fantastica')
+                    .should('have.text', 'Guarana Fantastica')
+                cy.get(ProductDetails.ProductInfo)
+                    .contains("Price: $ 4.50")
+                    .should('have.text', 'Price: $ 4.50')
+            })
+            //cy.reload()
+            //cy.ProductDetailsValidations('10').should('have.value', '10')
+            
+            /*cy.fixture('ProductDetails.json').then((ProductDetails)=>{
+                cy.get(ProductDetails.OrderButton).click()
+            })*/
+        })
     })  
-})
 
-describe('Product Details validations and actions', ()=>{
-    it('Validate some product value', ()=>{
-        cy.fixture('ProductDetails.json').then((ProductDetails)=>{
-            cy.get(ProductDetails.ProductName)
-                .contains('Guarana Fantastica')
-                .should('have.value', 'Guarana Fantastica')
-            cy.get(ProductDetails.ProductInfo)
-                .contains("Price: $ 4.50")
-                .should('have.text', 'Price: $ 4.50')
-        })
-        //cy.reload()
-        cy.ProductDetailsValidations('10').should('have.value', '10')
-        cy.fixture('ProductDetails.json').then((ProductDetails)=>{
-            cy.get(ProductDetails.OrderButton).click()
-        })
-    })
 })
--- cypress/support/login.js
+++ cypress/support/login.js
@@ -5,6 +5,6 @@ Cypress.Commands.add('login_pega', (mail, password)=>{
     cy.fixture('login.json').then((locators) =>{
         cy.get(locators.UserField).type(mail)
         cy.get(locators.PassField).type(password)
-        cy.get(locators.SignInButton).click()
+        cy.get(locators.SignInButton).click() 
     })
 })
