baee7c198c9dd14673236440a4d635761cbbb114
rafaelarthurov <rafael.arthurov@gmail.com>
2023-01-10 20:37:15 -0400

    Adding more asserts on HomePage

--- cypress/e2e/TestSuite.cy.js
+++ cypress/e2e/TestSuite.cy.js
@@ -14,52 +14,75 @@ var pass = '12345'
     })
 })*/
 
-describe('Pega Cloud e2e test', () =>{
+describe('Pega Cloud e2e test', () => {
 
     //###################################################
-    context('Login on Pega Cloud', ()=>{
-        before (()=>{
+    // Login Page
+    context('Login page on Training OpenSpan', () => {
+        before(() => {
             cy.visit('/')
         })
-        it('Validating CSS properties', ()=>{
-            cy.fixture('login.json').then((login)=>{
+        it('Validating CSS properties', () => {
+            cy.fixture('login').then((login) => {
+                //Logo size
                 cy.get(login.PageLogo)
                     .should('have.css', 'width', '100px')
                     .and('have.css', 'height', '47px')
+                //Header Background Color
                 cy.get(login.LoginHeader)
                     .should('have.css', 'background-color', 'rgb(31, 37, 85)')
-            })                
+            })
         })
-        it('Assert the state of the elements', ()=>{
-            cy.fixture('login.json').then((login)=>{
+        it('Assert the state of the elements', () => {
+            cy.fixture('login').then((login) => {
+                //Login button disabled
                 cy.get(login.SignInButton)
                     .should('be.disabled')
             })
         })
-        it('Login on PegaCloud', ()=>{
+        it('Login on PegaCloud', () => {
+            //Login CustomCommand
             cy.login_pega(user, pass)
         })
     })
 
     //###################################################
-    context('HomePage validations and actions', ()=>{
-        it('Validate the News Alert message', ()=>{
-              cy.fixture('homePage.json').then((homePage)=>{
-                  cy.get(homePage.NewsAlert)
-                      .contains('All packages shipped via DHL on May 20, 2013 experienced delayed delivery dates of up to 24 hours.')
-              })
-          })
-          it("Do a HomePage's product searching", ()=>{
-              cy.reload()
-              cy.homePage_search('1', '24')
-          })  
-      })
+    //Home Page
+    context('HomePage validations and actions', () => {
+        it('Validate the News Alert message', () => {
+            cy.fixture('homePage').then((homePage) => {
+                //Validate the news alert content
+                cy.get(homePage.NewsAlert)
+                    .contains('All packages shipped via DHL on May 20, 2013 experienced delayed delivery dates of up to 24 hours.')
+                cy.get(homePage.ProductType)
+                    .should('have.length', 1)
+                cy.get(homePage.ProductList)
+                    .should('have.length', 1)
+            })
+        })
+        it('Validate links code response', () => {
+            //Validate response 200 for each main menu link
+            cy.fixture('homePage').then((homePage) => {
+                cy.get(homePage.MainMenu_Home)
+                    .each(($el) => {
+                        cy.request($el.prop('href'))
+                            .its('status')
+                            .should('eq', 200)
+                    })
+            })
+        })
+        it("Do a HomePage's product searching", () => {
+            cy.reload()
+            //Seach Product CustomCommand
+            cy.homePage_search('1', '24')
+        })
+    })
 
     //###################################################
-
-    context('Product Details validations and actions', ()=>{
-        it('Validate some product value', ()=>{
-            cy.fixture('ProductDetails.json').then((ProductDetails)=>{
+    //Product Page
+    context('Product Details validations and actions', () => {
+        it('Validate some product value', () => {
+            cy.fixture('ProductDetails.json').then((ProductDetails) => {
                 cy.get(ProductDetails.ProductName)
                     .first()
                     .contains('Guarana Fantastica')
@@ -68,13 +91,6 @@ describe('Pega Cloud e2e test', () =>{
                     .contains("Price: $ 4.50")
                     .should('have.text', 'Price: $ 4.50')
             })
-            //cy.reload()
-            //cy.ProductDetailsValidations('10').should('have.value', '10')
-            
-            /*cy.fixture('ProductDetails.json').then((ProductDetails)=>{
-                cy.get(ProductDetails.OrderButton).click()
-            })*/
         })
-    })  
-
+    })
 })
